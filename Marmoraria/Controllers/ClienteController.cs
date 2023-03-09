d84d12cdb8ce11304f1aabbc00d70248f7aa966b
Caio Lucas <caio.lucas@ploomes.com>
2023-03-08 22:59:11 -0300

    Front of the customer register

---
+++ Marmoraria/Controllers/ClienteController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Marmoraria.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CadastrarCliente()
+        {
+            return View();
+        }
+
+        public IActionResult ExibirCliente()
+        {
+            return View();
+        }
+    }
+}
---
+++ Marmoraria/Controllers/PedidoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Marmoraria.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CadastrarPedido()
+        {
+            return View();
+        }
+
+        public IActionResult ExibirPedido()
+        {
+            return View();
+        }
+    }
+}
